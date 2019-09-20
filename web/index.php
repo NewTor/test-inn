a1234326b1c301a8e9762bde89ca4d7cd9aef73c
Alexander <aslesarev@inbox.ru>
2019-09-20 18:56:31 +0300

    описание задания

--- web/index.php
+++ web/index.php
@@ -1,1 +1,5 @@
 <?php
+$config = require __DIR__ . '/../config/config.php';
+require __DIR__ . '/../core/authenticity.php';
+$authenticity = new Authenticity();
+require __DIR__ . '/../view/view.php';
